fbd74eaca07daafaf324ae0ff052f2211d255152
DIntriligator <DIntriligator@gmail.com>
2016-11-09 15:46:08 -0500

    admin menu started

--- GW-Toolbelt-Main.php
+++ GW-Toolbelt-Main.php
@@ -7,7 +7,41 @@ Author: Graphics Westchester
 Version: 0.0.00
 */
 
+//define plugin directory constant
+define( 'PLUGIN_DIR', dirname(__FILE__).'/' );  
 
-function graphw_toolbox_menu() {
-add_menu_page( 'GW Toolbelt', 'GW Toolbelt', 'manage_options', 'graphicswestchestertoolbox', 'graphw_toolbox_options',plugin_dir_url(dirname(__FILE__)) . 'images/graphw-logo.png');
+/*
+*  gwtb_admin_menu
+*
+*  This function adds a menu that will allow addon plugins to create their own submenus
+*
+*  @type    function
+*  @date    11/09/16
+*  @since   0.0.00
+*
+*  @param   N/A
+*  @return  N/A
+*/
+
+function gwtb_admin_menu() {
+	add_menu_page( 'GW Toolbelt', 'GW Toolbelt', 'manage_options', 'gwtoolbelt', '', plugin_dir_url(__FILE__) . 'images/graphw-logo.png');
 }
+add_action( 'admin_menu', 'gwtb_admin_menu' );
+
+/*
+*  gwtb_admin_settings_menu
+*
+*  This function adds a settings section to view  
+*
+*  @type    function
+*  @date    11/09/16
+*  @since   0.0.00
+*
+*  @param   N/A
+*  @return  N/A
+*/
+
+function gwtb_admin_status_menu(){
+	add_submenu_page( 'gwtoolbelt', 'Status', 'Status', 'manage_options', 'gwtb-status', 'gwtb_status_init' )
+}
+add_action( 'admin_menu', 'gwtb_admin_menu' );
